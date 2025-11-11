6bd505dedc1ec1d6e43614700cfa8cce047a52a2
CMS2814 <cydhensabate14@gmail.com>
2025-11-10 21:32:00 -0500

    add utility functions for task management in Utilities.py

---
+++ Utilities.py
@@ -0,0 +1,21 @@
+# A module to put in utility functions such as add, view, edit, delete tasks and search tasks.
+
+
+def add_task(task):
+    pass
+
+
+def delete_task(task):
+    pass
+
+
+def edit_task(task):
+    pass
+
+
+def view_task(task):
+    pass
+
+
+def search_task(task):
+    pass
