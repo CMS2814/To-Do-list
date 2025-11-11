7e6db5f872039e4d8ba06a4e233869c3237d3d0e
CMS2814 <cydhensabate14@gmail.com>
2025-11-10 20:41:14 -0500

    initial commit: add main structure for to-do list app with modular functions

---
+++ Main.py
@@ -0,0 +1,11 @@
+# Do a to-do list app with modular functions
+# that allows users to add, view,edit, delete tasks and search tasks.
+# also use Json or txt file to save the tasks.
+
+# Things to learnd: File handling (open, write, read)
+
+# JSON basics (saving structured data)
+
+# Separation of logic (organizing functions)
+
+idk = 1
